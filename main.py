003afb360b0ce8df01e0d3b3f129590cd2111969
mplinthicum <mplinthicum@gmail.com>
2019-08-24 19:35:36 -0500

    move conf to dir, vet input against source list

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import argparse
 import configparser
 import json
+import sys
 from newsapi import NewsApiClient
+
+# read contents of a file
+# newline delimited
+# strip newline and store in array
+def read_file(file_path):
+    f = open(sources_file, 'r')
+    lines = f.readlines()
+    contents = []
+    for line in lines:
+        contents.append(line.strip('\n'))
+    return contents
 
 # read api key from config file
 config = configparser.ConfigParser()
-config.read('api.config')
+config.read('conf/api.config')
 api_key = config['api']['api_key']
+
+# read supported sources and countries from config
+config.read('conf/sources.config')
+sources_file = config['paths']['sources']
+countries_file = config['paths']['countries']
+
+sources = read_file(sources_file)
+countries = read_file(countries_file)
 
 # command line parser
 parser = argparse.ArgumentParser()
@@ -17,6 +37,9 @@ args = parser.parse_args()
 
 site = args.site
 query = args.query
+
+if site not in sources:
+    sys.exit('Source ' + site + ' is not supported\nPlease see list of supported sources at https://newsapi.org/sources')
 
 # call api
 news = NewsApiClient(api_key=api_key)
